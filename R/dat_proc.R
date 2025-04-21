378de304183678b94c93b7d7e68d8d52b0c01a30
fawda123 <mbafs2012@gmail.com>
2025-04-21 11:24:00 -0400

    addl processing for cba and lakewatch data

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -39,6 +39,7 @@ rawdat1 <- read_sheet('16_B7XLMDDgL-4RDz4UaFE4Gk569tYi2xaf1f96mAauY', na = c('.'
 dat1 <- rawdat1 |> 
   clean_names() |> 
   rename(
+    waterbody = water_body,
     station = station_number, 
     date = date_month_day_year, 
     time = time_24hr,
@@ -61,11 +62,61 @@ dat1 <- rawdat1 |>
   ) |> 
   mutate(
     date = as.Date(date),
-    time = gsub('^.*\\s', '', time),
+    county = case_when(
+      county == 'okaloosa' ~ 'Okaloosa',
+      county == 'walton' ~ 'Walton',
+      T ~ county
+    ), 
+    waterbody = gsub('creek', 'Creek', waterbody),
+    waterbody = gsub('beach', 'Beach', waterbody),
+    waterbody = gsub('FT\\.', 'Ft.', waterbody),
+    waterbody = gsub('GAP', 'Gap', waterbody), 
+    waterbody = gsub('walton', 'Walton', waterbody), 
+    waterbody = gsub('Tky', 'TKY', waterbody),
+    waterbody = gsub('Redfish', 'Red Fish', waterbody), 
+    waterbody = gsub('Cba', 'CBA', waterbody), 
+    waterbody = gsub('^Bass\\sLake$', 'Bass', waterbody),
   ) |> 
-  select(-month, -day, -year)
+  select(-month, -day, -year, -time)
+
+cbadat <- dat1
+
+save(cbadat, file = here('data', 'cbadat.RData'))
 
 # nutrient data
 rawdat2 <- read_sheet('1h4yvi9AnISVFbH_AvBw7wDx7s5-4VIOdqD-VToExmvg', na = c('NA', ''))
 
-dat2 <- rawdat2
+dat2 <- rawdat2 |> 
+  clean_names() |> 
+  rename(
+    waterbody = lake, 
+    tp_mgl = tp_mg_l, # us labelled as mg with clean_names, to convert
+    tn_mgl = tn_mg_l, # us labelled as mg with clean_names, to convert
+    chluncorr_ugl = chl_mg_l_uncorrected, # us labelled as mg with clean_names, no convert
+    chlcorr_ugl = chl_mg_l_corrected, # us labelled as mg with clean_names, no convert
+    secchi_onbott = secchi_2,
+    color_ptco = color_pt_co_units,
+    cond_uscm = cond_m_s_cm, # us
+    cond_mscm = cond_m_s_cm_2 # ms
+  ) |> 
+  mutate(
+    date = as.Date(date),
+    station = unlist(station), 
+    tp_mgl = tp_mgl / 1000,
+    tn_mgl = tn_mgl / 1000, 
+    cond_mscm = case_when(
+      is.na(cond_mscm) & !is.na(cond_uscm) ~ cond_uscm / 1000, 
+      T ~ cond_mscm
+    ), 
+    secchi_ft = case_when(
+      is.na(secchi_ft) & !is.na(secchi_onbott) ~ as.numeric(gsub('^.*\\(|\\).*$|Weeds|Bottom', '', secchi_onbott)), 
+      T ~ secchi_ft
+    ), 
+    secchi_onbott = grepl('Weeds|Bottom', secchi_onbott)
+  ) |> 
+  select(-month, -day, -year, -cond_uscm) |> 
+  select(county, waterbody,  station,  date, everything())
+
+lkwdat <- dat2
+
+save(lkwdat, file = here('data', 'lkwdat.RData'))
