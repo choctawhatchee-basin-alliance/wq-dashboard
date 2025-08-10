1aa669536149cb87285c12a4c07d5458beda568d
fawda123 <mbafs2012@gmail.com>
2025-08-10 10:48:30 -0400

    save helper data

--- tests/testthat/helper-data.R
+++ tests/testthat/helper-data.R
@@ -9,6 +9,8 @@ load(file = here('wq-dashboard/data/cntdat.RData'))
 load(file = here('wq-dashboard/data/meta.RData'))
 load(file = here('wq-dashboard/data/cbawbid.RData'))
 load(file = here('wq-dashboard/data/nncdat.RData'))
+load(file = here('wq-dashboard/data/raindat.RData'))
+load(file = here('wq-dahsboard/data/rainstas.RData'))
 
 dtrng <- c(as.Date('1992-11-08'), Sys.Date())
 stasbbox <- c(xmin = -86.78747, ymin = 30.2815, xmax = -85.68101, ymax = 31.10925) |> 
